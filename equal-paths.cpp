46d923f1c350c227312dfa816e2e81426379a7a9
Sophie Wang <swang588@usc.edu>
2025-11-20 07:14:55 +0000

    equal paths commit

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,25 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+// Returns the height if valid, or -1 if mismatch is found.
+int checkHeight(Node* root)
+{
+    if (root == nullptr)
+        return 0;
+
+    int lh = checkHeight(root->left);
+    int rh = checkHeight(root->right);
+
+    if (lh == -1 || rh == -1)
+        return -1;
+
+    if (root->left != nullptr && root->right != nullptr && lh != rh)
+        return -1;
+
+    return 1 + (lh > rh ? lh : rh);
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+    return checkHeight(root) != -1;
 }
-
